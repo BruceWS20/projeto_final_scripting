01793aff82aa077b2c6e2f376a97364e9c6259c4
BruceWS20 <31069798+BruceWS20@users.noreply.github.com>
2023-12-08 11:34:19 +0000

    projeto actualizado

--- projeto_final_scripting.sh
+++ projeto_final_scripting.sh
@@ -1,31 +1,32 @@
+ # Script para criar passwords aleatórias
  generate_password() {
     length=$1
     password=""
     
     for ((i=0; i<$length; i++))
     do
-       
+       # Gera automáticamente um número aleatório para decidirmos se o próximo caractere é um número ou não
         random=$((RANDOM % 2))
 
         if [ $random -eq 0 ]; then
-            
+            # Gera automáticamente um número entre 0 e 9
             password="${password}$(($RANDOM % 10))"
         else
-           
-            password="${password}$(printf \\$(printf '%03o' $((RANDOM % 26 + 65))))"
+           # Gera autmáticamente um caractere aleatório da tabela ASCII entre 65 e 125 (Maiúsculas, minúsculas e simbolos)
+            password="${password}$(printf \\$(printf '%03o' $((RANDOM % 60 + 65))))"
         fi
     done
     
     echo "$password"
 }
 
-
+# Leitura dos dados
 read -p "Número de caracteres: " length
 read -p "Quantas passwords?: " count
 
 echo "A criar $count passwords com número de caracteres $length..."
 
-
+# Output do Script
 for ((i=0; i<$count; i++))
 do
     password=$(generate_password $length)
